9425fd6036f5d4125e2cc246be69cffaa60b79f7
huanghaobin <huanghaobin@maomaokeji.cn>
2019-11-18 18:06:45 +0800

    static

--- main.go
+++ main.go
@@ -3,9 +3,19 @@ package main
 import (
 	_ "bzdx-tmp/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
 )
 
 func main() {
+	if beego.BConfig.RunMode == "dev" {
+		var FilterNoCache = func(ctx *context.Context) {
+			ctx.Output.Header("Cache-Control", "no-cache, no-store, must-revalidate")
+			ctx.Output.Header("Pragma", "no-cache")
+			ctx.Output.Header("Expires","0")
+		}
+
+		beego.InsertFilter("/static/*",beego.BeforeStatic, FilterNoCache)
+	}
 	beego.Run()
 }
 
